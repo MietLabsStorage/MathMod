61164e0ddac20f3c1e0366a3f125288775f90313
mJaJksJ <maksim.m00@mail.ru>
2021-10-26 20:07:32 +0300

    мнк

--- Лаб3/PRO.m
+++ Лаб3/PRO.m
@@ -6,8 +6,11 @@ v_en = 1000;
 alef_en = pi/4;
 location = 75000;
 minDiap = 10000;
-eps = 200;
+maxDiap = 20000;
+eps = 10;
 t_solve = 5;
+g = 9.8;
+t_iter = 0.01;
 
 t=0;
 index = 1;
@@ -22,13 +25,17 @@ t_pusk = 0;
 X_pusk =0;
 H_pusk =0;
 
+syms a
+syms b
+syms c
+
 while(true)
-    t=t+0.01;
+    t=t+t_iter;
     index=index+1;
     X_en(index) = v_en*cos(alef_en)*t;
-    H_en(index) = v_en*sin(alef_en)*t-9.8*t*t/2;
+    H_en(index) = v_en*sin(alef_en)*t-g*t*t/2;
 
-    if H_en(index) >= minDiap && X_en(index) < location/2
+    if H_en(index) >= minDiap && H_en(index) <= maxDiap && X_en(index) < location/2
         if t_last == 0
             t_last = t; 
         end
@@ -45,6 +52,14 @@ while(true)
                 X_pusk = X_en(index);
                 H_pusk = H_en(index);
                 t_last = 0;
+                
+                Q = 0;
+                for s = 1:1:newIndex-1
+                    Q = Q + (h_en(s) - a*x_en(s)*x_en(s)-b*x_en(s)-c)^2;                    
+                end                
+                coefs = solve(diff(Q, a),diff(Q, b),diff(Q, c));
+                syms x
+                coords = solve(double(coefs.a)*x*x+double(coefs.b)*x+double(coefs.c));
             end
         end
     end
@@ -65,11 +80,14 @@ if(doPlot)
     ylabel('высота, м')
     title('полет вражеских ракет')
 
-    plot(X_en, H_en)
-    plot(75000, 0, 'g+')
-    plot(100000, 0, 'gp')
+    plot(X_en, H_en, 'b')
+    plot(location, 0, 'k+', 'LineWidth', 2)
+    plot(100000, 0, 'kp', 'LineWidth', 2)
     plot(x_en, h_en,'*')
-    plot(X_pusk, H_pusk, '^')
+    plot(X_pusk, H_pusk, '^', 'LineWidth', 2)
     
-    legend('траектория врага', 'ПРО', 'штаб', 'снятые данные', 'вр. рак. в мом. пуск. наш.');
+    x=double(coords(1)):0.1:100000;
+    plot(x, double(coefs.a).*x.*x+double(coefs.b).*x+double(coefs.c), 'r');
+    
+    legend('траектория врага', 'ПРО', 'штаб', 'снятые данные', 'рак. в мом. пуск. наш.', 'мним.траект');
 end
